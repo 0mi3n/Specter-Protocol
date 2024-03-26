402e00364eaa23d5fbea88b706fd113f47e0925b
0mi3n <162841852+0mi3n@users.noreply.github.com>
2024-03-26 18:58:16 -0300

    Add files via upload

---
+++ bin/release.zsh
@@ -0,0 +1,37 @@
+set -e
+
+# Check if the first argument is either "staging", "testnet", or "mainnet"
+if [[ $1 != "staging" && $1 != "testnet" && $1 != "mainnet" ]]; then
+    echo "Error: First argument must be either 'staging', 'testnet', or 'mainnet'."
+    exit 1
+fi
+
+# Set variables
+aws_account_id="377928551571"
+aws_region="ca-central-1"
+ecr_repository_name="renegade-$1"
+image_name="renegade-relayer"
+image_tag="latest"
+
+# Log in to Amazon ECR
+aws ecr get-login-password --region $aws_region | \
+    docker login \
+        --username AWS \
+        --password-stdin \
+        $aws_account_id.dkr.ecr.$aws_region.amazonaws.com
+
+# Build the Docker image using buildkit for better caching
+export DOCKER_BUILDKIT=1
+docker build \
+    --platform linux/arm64 \
+    -t $image_name:$image_tag \
+    -f ./docker/release/Dockerfile \
+    .
+
+# Tag the Docker image for ECR
+docker tag $image_name:$image_tag $aws_account_id.dkr.ecr.$aws_region.amazonaws.com/$ecr_repository_name:$image_tag
+
+# Push the Docker image to ECR
+docker push $aws_account_id.dkr.ecr.$aws_region.amazonaws.com/$ecr_repository_name:$image_tag
+
+echo "Successfully released $1 build"
---
+++ bin/release_debug.zsh
@@ -0,0 +1,28 @@
+# Set variables
+aws_account_id="377928551571"
+aws_region="ca-central-1"
+ecr_repository_name="specter-main"
+image_name="specter-relayer-debug"
+image_tag="debug-latest"
+
+# Log in to Amazon ECR
+aws ecr get-login-password --region $aws_region | \
+    docker login \
+        --username AWS \
+        --password-stdin \
+        $aws_account_id.dkr.ecr.$aws_region.amazonaws.com
+
+# Build the Docker image using buildkit for better caching
+export DOCKER_BUILDKIT=1
+docker build \
+    -t $image_name:$image_tag \
+    -f ./docker/debug/Dockerfile \
+    .
+
+# Tag the Docker image for ECR
+docker tag $image_name:$image_tag $aws_account_id.dkr.ecr.$aws_region.amazonaws.com/$ecr_repository_name:$image_tag
+
+# Push the Docker image to ECR
+docker push $aws_account_id.dkr.ecr.$aws_region.amazonaws.com/$ecr_repository_name:$image_tag
+
+echo "Successfully released debug build"
